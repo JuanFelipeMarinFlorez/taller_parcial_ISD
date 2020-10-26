b2ded46f73822dbeb091d3a233dfb7f26d4a5b7d
Luis Manuel Peñaranda Ramirez <60193144+LuisPenaranda@users.noreply.github.com>
2020-10-26 11:00:37 -0500

    Add files via upload

---
+++ zero-pub-sub/src/main/java/com/pruebaps/graphic_interface.java
@@ -0,0 +1,71 @@
+package com.pruebacs;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import java.awt.Panel;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class graphic_interface {
+
+	private JFrame frame;
+	private JTextField escribir_mensaje;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					graphic_interface window = new graphic_interface();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public graphic_interface() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		
+		Panel panel = new Panel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		escribir_mensaje = new JTextField();
+		escribir_mensaje.setBounds(22, 228, 292, 20);
+		panel.add(escribir_mensaje);
+		escribir_mensaje.setColumns(10);
+		
+		JButton boton_enviar = new JButton("Enviar");
+		boton_enviar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		boton_enviar.setBounds(324, 227, 89, 23);
+		panel.add(boton_enviar);
+		
+		JScrollPane ver_mensaje = new JScrollPane();
+		ver_mensaje.setBounds(22, 11, 390, 206);
+		panel.add(ver_mensaje);
+	}
+}
