1faacf55be0ab191ec4235edb1f93c41c7273f36
Juan Felipe Marin Florez <juancho9964@hotmail.com>
2020-11-03 14:10:53 -0500

    3,0

--- zero-pub-sub/src/main/java/com/pruebaps/App.java
+++ zero-pub-sub/src/main/java/com/pruebaps/App.java
@@ -1,5 +1,7 @@
 package com.pruebaps;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	 System.out.println ("Por favor introduzca una cadena por teclado:");
+         String entradaTeclado = "";
+         Scanner entradaEscaner = new Scanner (System.in); //Creación de un objeto Scanner
+         entradaTeclado = entradaEscaner.nextLine (); //Invocamos un método sobre un objeto Scanner
+         System.out.println ("Entrada recibida por teclado es: \"" + entradaTeclado +"\"");
+       
+        if(entradaTeclado.equals("hola")) {
+        	 System.out.println( "Hello World!" );
+        	Publisher publicador= new Publisher();
+            String [] art={"Shakira","Juanes","Maluma","BadBunny"};
+            publicador.setArtistas(art);
+            publicador.newMessage("Shakira", "hola miamiiii");
+            publicador.publicar();
+            publicador.newMessage("Shakira", "waka waka");
+        }
+        else {
+        	Subscriber sub= new Subscriber();
+        	sub.newSubscription("Shakira");
+        	sub.subcripcion();
+        }
+        
     }
 }
