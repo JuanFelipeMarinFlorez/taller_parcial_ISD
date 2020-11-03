66f0319c0b1e2fc1e4e1ac999958b46d923bdf70
Paula Valentina Sánchez Peña <paula.valentinasp@gmail.com>
2020-11-03 15:02:54 -0500

    unión interfaces

--- zero-pub-sub/src/main/java/chats/chat_server.java
+++ zero-pub-sub/src/main/java/chats/chat_server.java
@@ -11,6 +11,12 @@ import javax.swing.JTextField;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import java.awt.Color;
+import javax.swing.JScrollPane;
+import java.awt.Component;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JSpinner;
 
 public class chat_server extends JFrame {
 
@@ -38,7 +44,7 @@ public class chat_server extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	private chat_server() {
+	public chat_server() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -47,21 +53,31 @@ public class chat_server extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JTextArea msg_area = new JTextArea();
-		msg_area.setBackground(new Color(255, 255, 255));
-		msg_area.setEditable(false);
-		msg_area.setBounds(44, 6, 350, 220);
-		contentPane.add(msg_area);
+		JScrollPane scrollingArea = new JScrollPane((Component) null);
+		scrollingArea.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollingArea.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scrollingArea.setBounds(6, 6, 438, 233);
+		contentPane.add(scrollingArea);
 		
 		msg_text = new JTextField();
-		msg_text.setBounds(39, 235, 270, 26);
+		msg_text.setBounds(6, 243, 274, 29);
 		contentPane.add(msg_text);
 		msg_text.setColumns(10);
 		
 		JButton msg_send = new JButton("send");
 		msg_send.setForeground(new Color(0, 0, 0));
 		msg_send.setBackground(new Color(199, 21, 133));
-		msg_send.setBounds(307, 235, 87, 29);
+		msg_send.setBounds(281, 244, 87, 29);
 		contentPane.add(msg_send);
+		
+		JTextArea msg_area = new JTextArea();
+		msg_area.setBounds(35, 8, 322, 211);
+		contentPane.add(msg_area);
+		msg_area.setEditable(false);
+		msg_area.setBackground(Color.WHITE);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(363, 244, 81, 28);
+		contentPane.add(spinner);
 	}
 }
---
+++ zero-pub-sub/src/main/java/chats/home.java
@@ -0,0 +1,87 @@
+package chats;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import java.awt.Component;
+import javax.swing.ScrollPaneConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class home extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtSeleccionaTuUsuario;
+
+	/**
+	 * Launch the application.
+	 */
+	
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					home frame = new home();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	private home() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 228, 225));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton msg_send = new JButton("Artista");
+		msg_send.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				chat_server artista = new chat_server();
+				artista.setVisible(true);
+			}
+		});
+		msg_send.setForeground(new Color(0, 0, 0));
+		msg_send.setBackground(new Color(199, 21, 133));
+		msg_send.setBounds(6, 121, 221, 100);
+		contentPane.add(msg_send);
+		
+		JButton msg_send_1 = new JButton("Suscriptor");
+		msg_send_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				chat_client suscriptor = new chat_client();
+				suscriptor.setVisible(true);
+			}
+		});
+		msg_send_1.setForeground(Color.BLACK);
+		msg_send_1.setBackground(new Color(199, 21, 133));
+		msg_send_1.setBounds(223, 121, 221, 100);
+		contentPane.add(msg_send_1);
+		
+		txtSeleccionaTuUsuario = new JTextField();
+		txtSeleccionaTuUsuario.setBackground(new Color(255, 204, 204));
+		txtSeleccionaTuUsuario.setEditable(false);
+		txtSeleccionaTuUsuario.setText(" Selecciona tu usuario");
+		txtSeleccionaTuUsuario.setBounds(148, 30, 156, 56);
+		contentPane.add(txtSeleccionaTuUsuario);
+		txtSeleccionaTuUsuario.setColumns(10);
+	}
+}
